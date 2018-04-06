3e7507084eb736af14824346e05b88eba8d1dcc8
HuyCV <cabhuy@gmail.com>
2018-04-06 13:59:24 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const express = require('express'),
+      path = require('path'),
+      http = require('http'),
+      bodyParser = require('body-parser'),
+
+      app = express(),
+      server = http.createServer(app),
+      
+      PORT = process.env.PORT || 3000
+      ;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.set('port', PORT);
+
+server.listen(PORT, () => {
+  console.log('App listening on port ' + PORT);
+})

6901ab49806d09a79ad5ec2f8feca425b5f7cecd
HuyCV <cabhuy@gmail.com>
2018-04-20 17:18:06 +0700

    Modified server.js

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ const express = require('express'),
       path = require('path'),
       http = require('http'),
       bodyParser = require('body-parser'),
+      mongoose = require('mongoose'),
 
       app = express(),
       server = http.createServer(app),
       
       PORT = process.env.PORT || 3000,
+      DB_PATH = 'mongodb://localhost/lunchdb',
 
       api = require('./app/routes/api')
       ;
+
+mongoose.connect(DB_PATH);
+mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
